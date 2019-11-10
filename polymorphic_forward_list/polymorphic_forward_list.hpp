450e0b1733437f144888b0a78c177b743674b181
Ted Stier <kerdek7@gmail.com>
2019-11-10 01:24:30 -0500

    drying

--- polymorphic_forward_list/polymorphic_forward_list.hpp
+++ polymorphic_forward_list/polymorphic_forward_list.hpp
@@ -1,7 +1,109 @@
 #ifndef POLYMORPHIC_FORWARD_LIST_HPP
 #define POLYMORPHIC_FORWARD_LIST_HPP
 
-#include <iterator>
+#include <iterator>	
+
+#define PFL_ERASE(a)													\
+	basic_node * trash = a;												\
+	a = trash->next;													\
+	delete trash;
+
+#define PFL_SPLICE(a, b)												\
+	basic_node * saved = a;												\
+	a = b;																\
+	b = a->next;														\
+	a->next = saved;
+
+#define PFL_SPLICE_WHILE(a, b, c, condition)							\
+	basic_node * saved = a->next;										\
+	a->next = b;														\
+	b = c;																\
+	while (condition) a = a->next;										\
+	a->next = saved;
+
+#define PFL_SWAP(a, b)													\
+	basic_node * saved = a;												\
+	a = b;																\
+	b = saved;
+
+#define PFL_MERGE(op)													\
+	if (this == &other) return;											\
+	if (!other.root.next) return;										\
+	link * pivot = &root;												\
+	for (; pivot->next && other.root.next; pivot = pivot->next)			\
+	{																	\
+		if (op)															\
+		{																\
+			PFL_SPLICE(pivot->next, other.root.next);					\
+		}																\
+	}																	\
+	if (other.root.next)												\
+	{																	\
+		PFL_SWAP(other.root.next, pivot->next);							\
+	}
+
+#define PFL_FOR_ALL(at, op)												\
+	while(at)															\
+	{																	\
+		op;																\
+	}
+
+#define PFL_ASSIGN(op, val)												\
+	using node_elem_type =												\
+		std::remove_const_t<std::remove_reference_t<decltype(val)>>;	\
+	link assign_root = nullptr;											\
+	link * assign_before_end = &assign_root;							\
+	try																	\
+	{																	\
+		op																\
+		{																\
+			assign_before_end =											\
+				new node<node_elem_type>(*assign_before_end, val);		\
+		}																\
+	}																	\
+	catch (...)															\
+	{																	\
+		PFL_FOR_ALL(assign_root.next, PFL_ERASE(assign_root.next))		\
+		throw;															\
+	}																	\
+	while (root.next)													\
+	{																	\
+		PFL_ERASE(root.next);											\
+	}																	\
+	root.next = assign_root.next;
+
+#define PFL_INSERT(op, val)												\
+	using node_elem_type =												\
+		std::remove_const_t<std::remove_reference_t<decltype(val)>>;	\
+	link insert_root;													\
+	link * insert_before_end = &insert_root;							\
+	try																	\
+	{																	\
+		op																\
+		{																\
+			insert_before_end											\
+				= new node<node_elem_type>(insert_before_end, val);		\
+		}																\
+	}																	\
+	catch (...)															\
+	{																	\
+		PFL_FOR_ALL(insert_root.next, PFL_ERASE(insert_root.next))		\
+		throw;															\
+	}																	\
+	PFL_SWAP(pos.p->next, insert_root.next);							\
+	return insert_before_end;
+
+#define PFL_REMOVE(op)													\
+	size_type removed_count = 0;										\
+	for (link * pivot = &root; pivot->next; pivot = pivot->next)		\
+	{																	\
+		if (op)															\
+		{																\
+			PFL_ERASE(pivot->next);										\
+			removed_count++;											\
+		}																\
+	}																	\
+	return removed_count;
 
 template<class Elem_Base>
 class polymorphic_forward_list
@@ -107,7 +209,7 @@ public:
 		}
 		auto operator++(int) noexcept -> iterator
 		{
-			auto copy = *this;
+			iterator copy = *this;
 			p = p->next;
 			return copy;
 		}
@@ -168,7 +270,7 @@ public:
 		}
 		auto operator++(int) noexcept -> const_iterator
 		{
-			auto copy = *this;
+			const_iterator copy = *this;
 			p = p->next;
 			return copy;
 		}
@@ -207,87 +309,24 @@ public:
 	auto operator=(polymorphic_forward_list && other) noexcept
 		-> polymorphic_forward_list &
 	{
-		basic_node * temp = root.next;
-		root.next = other.root.next;
-		other.root.next = temp;
+		PFL_SWAP(root.next, other.root.next);
 	}
 
 	~polymorphic_forward_list() noexcept
 	{
-		while (root.next)
-		{
-			basic_node * trash = root.next;
-			root.next = trash->next;
-			delete trash;
-		}
+		PFL_FOR_ALL(root.next, PFL_ERASE(root.next));
 	}
 
 	void assign(size_type count, const_reference value)
 	{
-		using node_elem_type =
-			std::remove_const_t<std::remove_reference_t<const_reference>>;
-		link assign_root = nullptr;
-		link * assign_before_end = &assign_root;
-		try
-		{
-			for (size_type i = 0; i < count; i++)
-			{
-				assign_before_end =
-					new node<node_elem_type>(*assign_before_end, value);
-			}
-		}
-		catch (...)
-		{
-			while (assign_root.next)
-			{
-				basic_node * trash = assign_root.next;
-				assign_root.next = trash->next;
-				delete trash;
-			}
-			throw;
-		}
-		while (root.next)
-		{
-			basic_node * trash = root.next;
-			root.next = trash->next;
-			delete trash;
-		}
-		root.next = assign_root.next;
+		PFL_ASSIGN(for (size_type i = 0; i < count; i++), value);
 	}
 
 	template<class InputIt>
 	auto assign(InputIt first, InputIt last)
 		-> std::enable_if_t<!std::is_integral_v<InputIt>>
 	{
-		using node_elem_type =
-			std::remove_const_t<std::remove_reference_t<decltype(*first)>>;
-		link assign_root = nullptr;
-		link * assign_before_end = &assign_root;
-		try
-		{
-			while (first != last)
-			{
-				assign_before_end =
-					new node<node_elem_type>(*assign_before_end, *first++);
-			}
-		}
-		catch (...)
-		{
-			while (assign_root.next)
-			{
-				basic_node * trash = assign_root.next;
-				assign_root.next = trash->next;
-				delete trash;
-			}
-			throw;
-		}
-		while (root.next)
-		{
-			basic_node * trash = root.next;
-			root.next = trash->next;
-			delete trash;
-		}
-		root.next = assign_root.next;
+		PFL_ASSIGN(while (first != last), *first++);
 	}
 
 	auto front() noexcept -> reference
@@ -350,12 +389,7 @@ public:
 
 	void clear() noexcept
 	{
-		while (root.next)
-		{
-			basic_node * trash = root.next;
-			root.next = trash->next;
-			delete trash;
-		}
+		PFL_FOR_ALL(root.next, PFL_ERASE(root.next));
 	}
 
 	template<class Elem_Derived>
@@ -377,62 +411,14 @@ public:
 		size_type count,
 		Elem_Derived const & value) -> iterator
 	{
-		link splice_root;
-		link * splice_before_end = &splice_root;
-		try
-		{
-			for (size_type i = 0; i < count; i++)
-			{
-				splice_before_end
-					= new node<Elem_Derived>(splice_before_end, value);
-			}
-		}
-		catch (...)
-		{
-			while (splice_root.next)
-			{
-				basic_node * trash = splice_root.next;
-				splice_root.next = trash->next;
-				delete trash;
-			}
-			throw;
-		}
-		link * saved = pos.p->next;
-		pos.p->next = splice_root.next;
-		splice_before_end.p->next = saved;
-		return splice_before_end;
+		PFL_INSERT(for (size_type i = 0; i < count; i++), value);
 	}
 
 	template<class InputIt>
 	auto insert_after(const_iterator pos, InputIt first, InputIt last)
 		-> std::enable_if_t<!std::is_integral_v<InputIt>, iterator>
 	{
-		using node_elem_type =
-			std::remove_const_t<std::remove_reference_t<decltype(*first)>>;
-		link splice_root;
-		link * splice_before_end = &splice_root;
-		try
-		{
-			while (first != last)
-			{
-				splice_before_end =
-					new node<node_elem_type>(splice_before_end, *first++);
-			}
-		}
-		catch (...)
-		{
-			while (splice_root.next)
-			{
-				basic_node * trash = splice_root.next;
-				splice_root.next = trash->next;
-				delete trash;
-			}
-			throw;
-		}
-		link * saved = pos.p->next;
-		pos.p->next = splice_root.next;
-		splice_before_end.p->next = saved;
-		return splice_before_end;
+		PFL_INSERT(while (first != last), *first++);
 	}
 
 	template<class Elem_Derived = Elem_Base, class ... Args>
@@ -443,21 +429,14 @@ public:
 
 	auto erase_after(const_iterator pos) noexcept
 	{
-		basic_node * trash = pos.p->next;
-		pos.p->next = trash->next;
-		delete trash;
+		PFL_ERASE(pos.p->next)
 		return pos.p->next;
 	}
 
 	auto erase_after(const_iterator first, const_iterator last) noexcept
 		-> iterator
 	{
-		while ((first.p->next) != last.p)
-		{
-			basic_node * trash = first.p->next;
-			first.p->next = trash->next;
-			delete trash;
-		}
+		PFL_FOR_ALL((first.p->next) != last.p, PFL_ERASE(first.p->next));
 		return last.p;
 	}
 
@@ -484,127 +463,44 @@ public:
 	void merge(polymorphic_forward_list & other)
 		noexcept(noexcept(other.root.next->ref < root.next->ref))
 	{
-		if (this == &other) return;
-		if (!other.root.next) return;
-		link * pivot = &root;
-		for (; pivot->next && other.root.next; pivot = pivot->next)
-		{
-			if (other.root.next->ref < pivot->next->ref)
-			{
-				basic_node * saved = pivot->next;
-				pivot->next = other.root.next;
-				other.root.next = pivot->next->next;
-				pivot->next->next = saved; // that's a rotate
-			}
-		}
-		if (other.root.next)
-		{
-			basic_node * saved = other.root.next;
-			other.root.next = pivot->next;
-			pivot->next = saved;
-		}
+		PFL_MERGE(other.root.next->ref < pivot->next->ref);
 	}
 
 	void merge(polymorphic_forward_list && other)
 		noexcept(noexcept(other.root.next->ref < root.next->ref))
 	{
-		if (this == &other) return;
-		if (!other.root.next) return;
-		link * pivot = &root;
-		for (; pivot->next && other.root.next; pivot = pivot->next)
-		{
-			if (other.root.next->ref < pivot->next->ref)
-			{
-				basic_node * saved = pivot->next;
-				pivot->next = other.root.next;
-				other.root.next = pivot->next->next;
-				pivot->next->next = saved;
-			}
-		}
-		if (other.root.next)
-		{
-			basic_node * saved = other.root.next;
-			other.root.next = pivot->next;
-			pivot->next = saved;
-		}
+		PFL_MERGE(other.root.next->ref < pivot->next->ref);
 	}
 
 	template<class Compare>
 	void merge(polymorphic_forward_list & other, Compare comp)
 		noexcept(noexcept(comp(other.root.next->ref, root.next->ref)))
 	{
-		if (this == &other) return;
-		if (!other.root.next) return;
-		link * pivot = &root;
-		for (; pivot->next && other.root.next; pivot = pivot->next)
-		{
-			if (comp(other.root.next->ref, pivot->next->ref))
-			{
-				basic_node * saved = pivot->next;
-				pivot->next = other.root.next;
-				other.root.next = pivot->next->next;
-				pivot->next->next = saved;
-			}
-		}
-		if (other.root.next)
-		{
-			basic_node * saved = other.root.next;
-			other.root.next = pivot->next;
-			pivot->next = saved;
-		}
+		PFL_MERGE(comp(other.root.next->ref, pivot->next->ref));
 	}
 
 	template<class Compare>
 	void merge(polymorphic_forward_list && other, Compare comp)
 		noexcept(noexcept(comp(other.root.next->ref, root.next->ref)))
 	{
-		if (this == &other) return;
-		if (!other.root.next) return;
-		link * pivot = &root;
-		for (; pivot->next && other.root.next; pivot = pivot->next)
-		{
-			if (comp(other.root.next->ref, pivot->next->ref))
-			{
-				basic_node * temp = pivot->next;
-				pivot->next = other.root.next;
-				other.root.next = pivot->next->next;
-				pivot->next->next = temp;
-			}
-		}
-		if (other.root.next)
-		{
-			basic_node * saved = other.root.next;
-			other.root.next = pivot->next;
-			pivot->next = saved;
-		}
+		PFL_MERGE(comp(other.root.next->ref, pivot->next->ref));
 	}
 
 	void splice_after(const_iterator pos, polymorphic_forward_list & other)
 		noexcept
 	{
-		basic_node * saved = pos.p->next;
-		pos.p->next = other.root.next;
-		other.root.next = nullptr;
-		while (pos.p->next) pos.p = pos.p->next;
-		pos.p->next = saved;
+		PFL_SPLICE_WHILE(pos.p, other.root.next, nullptr, pos.p->next);
 	}
 
 	void splice_after(const_iterator pos, polymorphic_forward_list && other)
 		noexcept
 	{
-		basic_node * saved = pos.p->next;
-		pos.p->next = other.root.next;
-		other.root.next = nullptr;
-		while (pos.p->next) pos.p = pos.p->next;
-		pos.p->next = saved;
+		PFL_SPLICE_WHILE(pos.p, other.root.next, nullptr, pos.p->next);
 	}
 
 	void splice_after(const_iterator pos, const_iterator it) noexcept
 	{
-		basic_node * saved = pos.p->next;
-		pos.p->next = it.p->next;
-		it.p->next = pos.p->next->next;
-		pos.p->next->next = saved;
+		PFL_SPLICE(pos.p->next, it.p->next);
 	}
 
 	void splice_after(
@@ -612,63 +508,29 @@ public:
 		const_iterator first,
 		const_iterator last) noexcept
 	{
-		basic_node * saved = pos.p->next;
-		pos.p->next = first.p->next;
-		first.p->next = last.p;
-		while (pos.p->next != last.p) pos.p = pos.p->next;
-		pos.p->next = saved;
+		PFL_SPLICE_WHILE(pos.p, first.p->next, last.p, pos.p->next != last.p);
 	}
 
 	void swap(polymorphic_forward_list & other) noexcept
 	{
-		basic_node * saved = root.next;
-		root.next = other.root.next;
-		other.root.next = saved;
+		PFL_SWAP(root.next, other.root.next);
 	}
 
 	auto remove(const_reference value) -> size_type
 	{
-		size_type removed_count = 0;
-		for (link * pivot = &root; pivot->next; pivot = pivot->next)
-		{
-			if (pivot->next->ref == value)
-			{
-				basic_node * trash = pivot->next;
-				pivot->next = trash->next;
-				delete trash;
-				removed_count++;
-			}
-		}
-		return removed_count;
+		PFL_REMOVE(pivot->next->ref == value);
 	}
 
 	template<class UnaryPredicate>
 	auto remove_if(UnaryPredicate p) -> size_type
 	{
-		size_type removed_count = 0;
-		for (link * pivot = &root; pivot->next; pivot = pivot->next)
-		{
-			if (p(pivot->next->ref))
-			{
-				basic_node * trash = pivot->next;
-				pivot->next = trash->next;
-				delete trash;
-				removed_count++;
-			}
-		}
-		return removed_count;
+		PFL_REMOVE(p(pivot->next->ref));
 	}
 
 	void reverse() noexcept
 	{
 		link reverse_root = nullptr;
-		while (root.next)
-		{
-			basic_node * saved = reverse_root.next;
-			reverse_root.next = root.next;
-			root.next = reverse_root.next->next;
-			reverse_root.next->next = saved;
-		}
+		PFL_FOR_ALL(root.next, PFL_SPLICE(reverse_root.next, root.next));
 		root.next = reverse_root.next;
 	}
 
@@ -806,5 +668,15 @@ auto operator<=(
 		if (*left++ < *right++) return true;
 	}
 }
+
+#undef PFL_ERASE
+#undef PFL_SPLICE
+#undef PFL_SPLICE_WHILE
+#undef PFL_SWAP
+#undef PFL_MERGE
+#undef PFL_FOR_ALL
+#undef PFL_ASSIGN
+#undef PFL_INSERT
+#undef PFL_REMOVE
 
 #endif
